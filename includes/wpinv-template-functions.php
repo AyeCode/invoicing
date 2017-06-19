c8a891f6d6c8b637c5e0c6776bbb6f78621ae236
Kiran Prajapati <kprajapati@thakkertech.com>
2017-06-19 19:48:56 +0530

    changes for custom item uintegrations

--- includes/wpinv-template-functions.php
+++ includes/wpinv-template-functions.php
@@ -1067,6 +1067,7 @@ function wpinv_display_line_items( $invoice_id = 0 ) {
                         $post_link = '<a href="' . get_edit_post_link( $cart_item['meta']['post_id'] ) .'" target="_blank">' . (!empty($cart_item['meta']['invoice_title']) ? $cart_item['meta']['invoice_title'] : get_the_title( $cart_item['meta']['post_id']) ) . '</a>';
                         $summary = wp_sprintf( __( '%s: %s', 'invoicing' ), $item->get_cpt_singular_name(), $post_link );
                     }
+                    $summary = apply_filters( 'wpinv_print_invoice_line_item_summary', $summary, $cart_item, $item, $invoice );
                     
                     $item_tax       = '';
                     $tax_rate       = '';
@@ -1317,6 +1318,7 @@ function wpinv_admin_get_line_items($invoice = array()) {
             $post_link = '<a href="' . get_edit_post_link( $cart_item['meta']['post_id'] ) .'" target="_blank">' . (!empty($cart_item['meta']['invoice_title']) ? $cart_item['meta']['invoice_title'] : get_the_title( $cart_item['meta']['post_id']) ) . '</a>';
             $summary = wp_sprintf( __( '%s: %s', 'invoicing' ), $wpi_item->get_cpt_singular_name(), $post_link );
         }
+        $summary = apply_filters( 'wpinv_admin_invoice_line_item_summary', $summary, $cart_item, $wpi_item, $invoice );
         
         $item_tax       = '';
         $tax_rate       = '';
