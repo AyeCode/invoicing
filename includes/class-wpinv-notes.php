726cbf5822abc79a1dbbbe9ac2b51206e20e6378
Kiran Prajapati <kprajapati@thakkertech.com>
2017-07-18 18:20:26 +0530

    Invoice notes should not counted in WP standard comments count - CHANGED

--- includes/class-wpinv-notes.php
+++ includes/class-wpinv-notes.php
@@ -25,6 +25,13 @@ class WPInv_Notes {
         // Secure inovice notes
         add_action( 'pre_get_comments', array( $this, 'set_invoice_note_type' ), 11, 1 );
         
+        // Count comments
+        add_filter( 'wp_count_comments', array( $this, 'wp_count_comments' ), 11, 2 );
+        
+        // Delete comments count cache whenever there is a new comment or a comment status changes
+        add_action( 'wp_insert_comment', array( $this, 'delete_comments_count_cache' ) );
+        add_action( 'wp_set_comment_status', array( $this, 'delete_comments_count_cache' ) );
+        
         do_action( 'wpinv_class_notes_actions', $this );
     }
         
@@ -74,5 +81,70 @@ class WPInv_Notes {
         $args   = apply_filters( 'wpinv_invoice_notes_args', $args, $this, $invoice_id, $type );
         
         return get_comments( $args );
+    }
+    
+    /**
+     * Delete comments count cache whenever there is new comment or the 
+     * status of a comment changes. Cache will be regenerated next time 
+     * WPInv_Notes::wp_count_comments() is called.
+     *
+     * @return void
+     */
+    public function delete_comments_count_cache() {
+        delete_transient( 'wpinv_count_comments' );
+    }
+    
+    /**
+     * Remove invoice notes from wp_count_comments().
+     *
+     * @since  1.0.0
+     * @param  object $stats   Comment stats.
+     * @param  int    $post_id Post ID.
+     * @return object
+     */
+    public function wp_count_comments( $stats, $post_id ) {
+        global $wpdb;
+
+        if ( 0 === $post_id ) {
+            $stats = get_transient( 'wpinv_count_comments' );
+
+            if ( ! $stats ) {
+                $stats = array();
+
+                $count = $wpdb->get_results( "SELECT comment_approved, COUNT(*) AS num_comments FROM {$wpdb->comments} WHERE comment_type NOT IN ('" . $this->comment_type . "') GROUP BY comment_approved", ARRAY_A );
+
+                $total = 0;
+                $approved = array(
+                    '0'            => 'moderated',
+                    '1'            => 'approved',
+                    'spam'         => 'spam',
+                    'trash'        => 'trash',
+                    'post-trashed' => 'post-trashed',
+                );
+
+                foreach ( (array) $count as $row ) {
+                    // Do not count post-trashed toward totals.
+                    if ( 'post-trashed' !== $row['comment_approved'] && 'trash' !== $row['comment_approved'] ) {
+                        $total += $row['num_comments'];
+                    }
+                    if ( isset( $approved[ $row['comment_approved'] ] ) ) {
+                        $stats[ $approved[ $row['comment_approved'] ] ] = $row['num_comments'];
+                    }
+                }
+
+                $stats['total_comments'] = $total;
+                $stats['all'] = $total;
+                foreach ( $approved as $key ) {
+                    if ( empty( $stats[ $key ] ) ) {
+                        $stats[ $key ] = 0;
+                    }
+                }
+
+                $stats = (object) $stats;
+                set_transient( 'wpinv_count_comments', $stats );
+            }
+        }
+
+        return $stats;
     }
 }
