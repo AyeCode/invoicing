c751c064f2fdea2a9f2264286ffe7d096dfb5014
picocodes <picocodes@gmail.com>
2021-02-05 15:25:51 +0300

    invoice tax not calculated correctly

--- includes/class-wpinv-invoice.php
+++ includes/class-wpinv-invoice.php
@@ -3477,9 +3477,8 @@ class WPInv_Invoice extends GetPaid_Data {
 			foreach ( $this->get_items() as $item ) {
 				$rates    = getpaid_get_item_tax_rates( $item, $this->get_country(), $this->get_state() );
 				$rates    = getpaid_filter_item_tax_rates( $item, $rates );
-				$taxes    = getpaid_calculate_item_taxes( $item->get_sub_total(), $rates );
-				$r_taxes  = getpaid_calculate_item_taxes( $item->get_recurring_sub_total(), $rates );
-
+				$taxes    = getpaid_calculate_item_taxes( getpaid_get_taxable_amount( $item->get_id(), $item->get_sub_total(), $this->get_discount_code(), false ), $rates );
+				$r_taxes  = getpaid_calculate_item_taxes( getpaid_get_taxable_amount( $item->get_id(), $item->get_recurring_sub_total(), $this->get_discount_code(), true ), $rates );
 				foreach ( $taxes as $name => $amount ) {
 					$recurring = isset( $r_taxes[ $name ] ) ? $r_taxes[ $name ] : 0;
 					$tax       = getpaid_prepare_item_tax( $item, $name, $amount, $recurring );
