0a3819dc0d5d64eb2d3a055df87bf9bb470b83c2
Sandeep Jain <mr.sandeepmcscet@gmail.com>
2021-03-30 18:53:49 +0530

    add invoice image sizes

--- templates/invoice/invoice-logo.php
+++ templates/invoice/invoice-logo.php
@@ -12,9 +12,20 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <a target="_blank" class="logo-link text-dark" href="<?php echo esc_url( wpinv_get_business_website() ); ?>">
 
-    <?php if ( $logo = wpinv_get_business_logo() ) { ?>
+    <?php if ( $logo = wpinv_get_business_logo() ) {
+        $logo_width = wpinv_get_option( 'logo_width' );
+        $logo_height = wpinv_get_option( 'logo_height' );
+        if ( ! empty( $logo_width ) && ! empty( $logo_height ) ) {
+            echo sprintf(
+               '<img class="logo" style="width:%spx; height:%spx" src="%s"/>',
+               esc_attr( $logo_width ),
+               esc_attr( $logo_height ),
+               esc_url( $logo )
+            );
+        } else {
+        ?>
         <img class="logo" style="max-width:100%;" src="<?php echo esc_url( $logo ); ?>">
-    <?php } else { ?>
+    <?php } } else { ?>
         <h1 class="h3"><?php echo esc_html( wpinv_get_business_name() ); ?></h1>
     <?php } ?>
 
