e6d0e0a3b874531203abe821f02cb98de76495cb
stiofan <info@wpgeodirectory.com>
2021-06-24 15:54:37 +0100

    wizard last step tweaks

--- includes/admin/views/wizard-thank-you.php
+++ includes/admin/views/wizard-thank-you.php
@@ -18,24 +18,39 @@ defined( 'ABSPATH' ) || exit;
 
 		<div class="gp-setup-next-steps">
 			<div class="gp-setup-next-steps-first mb-4">
-				<h2 class="h3"><?php esc_html_e( 'Next steps', 'invoicing' ); ?></h2>
-					<a
-						class="btn btn-primary btn-sm"
-						href="<?php echo esc_url( admin_url( 'post-new.php?post_type=wpi_item' ) ); ?>"><?php esc_html_e( 'Create your first Item!', 'invoicing' ); ?></a>
-					<div class="gp-setup-next-steps-first mb-4">
-						<h2 class="h3"><?php esc_html_e( 'Examples', 'invoicing' ); ?></h2>
-						<a
-							class="btn btn-primary btn-sm"
-							target="_blank"
-							href="https://demos.ayecode.io/getpaid/"><?php esc_html_e( "View What's Possible", 'invoicing' ); ?></a>
-
-						<a
-							class="btn btn-outline-primary btn-sm"
-							target="_blank"
-							href="https://demos.ayecode.io/getpaid/"><?php esc_html_e( "View What's Possible", 'invoicing' ); ?></a>
+				<div>
+					<h2 class="h3"><i class="fas fa-arrow-down"></i> <?php esc_html_e( 'Watch This', 'invoicing' ); ?> <i class="fas fa-arrow-down"></i></h2>
+					<div class="embed-responsive embed-responsive-16by9 mb-4">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/TXZuPXHjt9E?rel=0" allowfullscreen></iframe>
 					</div>
-					<div class="gp-setup-next-steps-last">
-						<h2 class="h3"><?php _e( 'Learn more', 'invoicing' ); ?></h2>
+				</div>
+				<h2 class="h3"><?php esc_html_e( 'Next steps', 'invoicing' ); ?></h2>
+
+				<div class="d-flex justify-content-between">
+					<a
+						class="btn btn-outline-primary btn-sm"
+						href="<?php echo esc_url( admin_url( 'post-new.php?post_type=wpi_item' ) ); ?>">
+						<span class="h1 d-block"><i class="fas fa-box-open"></i></span>
+						<?php esc_html_e( 'Create Item', 'invoicing' ); ?>
+					</a>
+					<a
+						class="btn btn-outline-primary btn-sm"
+						href="<?php echo esc_url( admin_url( 'post-new.php?post_type=wpi_item' ) ); ?>">
+						<span class="h1 d-block"><i class="fas fa-align-justify"></i></span>
+						<?php esc_html_e( 'Create Payment Form', 'invoicing' ); ?>
+					</a>
+					<a
+						class="btn btn-outline-primary btn-sm"
+						href="<?php echo esc_url( admin_url( 'post-new.php?post_type=wpi_item' ) ); ?>">
+						<span class="h1 d-block"><i class="fas fa-file-alt"></i></span>
+						<?php esc_html_e( 'Create Invoice', 'invoicing' ); ?>
+					</a>
+				</div>
+
+
+
+						<h2 class="h3 mt-4"><?php _e( 'Learn more', 'invoicing' ); ?></h2>
+				<div class="gp-setup-next-steps-last mt-2 d-flex justify-content-between">
 						<a
 							class="btn btn-outline-primary btn-sm" href="https://docs.wpgetpaid.com/collection/114-getting-started?utm_source=setupwizard&utm_medium=product&utm_content=getting-started&utm_campaign=invoicingplugin"
 							target="_blank"><?php esc_html_e( 'Getting Started', 'invoicing' ); ?></a>
