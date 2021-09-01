ae3eec7dda78c38384d74777acae4023e886b408
picocodes <picocodes@gmail.com>
2021-09-01 12:09:03 +0300

    check for getpaid-* when loading scripts

--- includes/admin/class-getpaid-admin.php
+++ includes/admin/class-getpaid-admin.php
@@ -101,7 +101,7 @@ class GetPaid_Admin {
         }
 
         // General styles.
-        if ( false !== stripos( $page, 'wpi' ) || 'gp-setup' == $page ) {
+        if ( false !== stripos( $page, 'wpi' ) || false !== stripos( $page, 'getpaid' ) || 'gp-setup' == $page ) {
 
             // Styles.
             $version = filemtime( WPINV_PLUGIN_DIR . 'assets/css/admin.css' );
