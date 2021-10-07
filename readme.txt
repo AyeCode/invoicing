4e36e9db7a1752f3c13bb096f96e1947bdd4c2ac
stiofan <info@wpgeodirectory.com>
2021-10-07 20:51:33 +0100

    2.5.1

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.ko-fi.com/stiofan
 Tags:  payments, invoice, invoicing, Recurring payments, Paypal, Stripe, credit card payments, buy now button, checkout form, payments form builder,
 Requires at least: 4.9
 Tested up to: 5.8
-Stable tag: 2.5.0
+Stable tag: 2.5.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -132,6 +132,9 @@ Automatic updates should seamlessly work. We always suggest you backup up your w
 9. Single invoice view page.
 
 == Changelog ==
+
+= 2.5.1 =
+* Prevent error if Elementor Pro installed and old extension not updated - FIXED
 
 = 2.5.0 =
 * Bank transfer gateway now supports subscriptions - ADDED
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,101 +1,98 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '991cb4f4e9a24c6fb631cabf4202897af8758a3a',
+        'name' => 'ayecode/invoicing',
+        'dev' => true,
     ),
-    'reference' => 'e9e8ae2fe4e6391f1dea016f5f84493ae729008e',
-    'name' => 'ayecode/invoicing',
-  ),
-  'versions' => 
-  array (
-    'ayecode/ayecode-connect-helper' => 
-    array (
-      'pretty_version' => '1.0.3',
-      'version' => '1.0.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '1af7cdefdbd20d4443a3ab4834e4c1cd8fe57fb4',
+    'versions' => array(
+        'ayecode/ayecode-connect-helper' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/ayecode-connect-helper',
+            'aliases' => array(),
+            'reference' => '1af7cdefdbd20d4443a3ab4834e4c1cd8fe57fb4',
+            'dev_requirement' => false,
+        ),
+        'ayecode/invoicing' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '991cb4f4e9a24c6fb631cabf4202897af8758a3a',
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-ayecode-ui' => array(
+            'pretty_version' => '0.1.61',
+            'version' => '0.1.61.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-ayecode-ui',
+            'aliases' => array(),
+            'reference' => 'e97d1e520d6722df2cb323c04a6e818b1e901ab5',
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-deactivation-survey' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-deactivation-survey',
+            'aliases' => array(),
+            'reference' => 'c4b0ba914835f17dca0cf69fe621c2db491d4667',
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-font-awesome-settings' => array(
+            'pretty_version' => '1.0.13',
+            'version' => '1.0.13.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-font-awesome-settings',
+            'aliases' => array(),
+            'reference' => 'a7a11ee4290674ec214d1fe694139af275350402',
+            'dev_requirement' => false,
+        ),
+        'ayecode/wp-super-duper' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayecode/wp-super-duper',
+            'aliases' => array(),
+            'reference' => 'ffbd740273dde423206c13c680ee90202b334799',
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'maxmind-db/reader' => array(
+            'pretty_version' => 'v1.6.0',
+            'version' => '1.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maxmind-db/reader',
+            'aliases' => array(),
+            'reference' => 'febd4920bf17c1da84cef58e56a8227dfb37fbe4',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
     ),
-    'ayecode/invoicing' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e9e8ae2fe4e6391f1dea016f5f84493ae729008e',
-    ),
-    'ayecode/wp-ayecode-ui' => 
-    array (
-      'pretty_version' => '0.1.61',
-      'version' => '0.1.61.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e97d1e520d6722df2cb323c04a6e818b1e901ab5',
-    ),
-    'ayecode/wp-deactivation-survey' => 
-    array (
-      'pretty_version' => '1.0.3',
-      'version' => '1.0.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c4b0ba914835f17dca0cf69fe621c2db491d4667',
-    ),
-    'ayecode/wp-font-awesome-settings' => 
-    array (
-      'pretty_version' => '1.0.13',
-      'version' => '1.0.13.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'a7a11ee4290674ec214d1fe694139af275350402',
-    ),
-    'ayecode/wp-super-duper' => 
-    array (
-      'pretty_version' => '2.0.1',
-      'version' => '2.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '7844e43f00d2e8f32c5883e46bdc5154da5a5487',
-    ),
-    'composer/installers' => 
-    array (
-      'pretty_version' => 'v1.12.0',
-      'version' => '1.12.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
-    ),
-    'maxmind-db/reader' => 
-    array (
-      'pretty_version' => 'v1.6.0',
-      'version' => '1.6.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'febd4920bf17c1da84cef58e56a8227dfb37fbe4',
-    ),
-    'roundcube/plugin-installer' => 
-    array (
-      'replaced' => 
-      array (
-        0 => '*',
-      ),
-    ),
-    'shama/baton' => 
-    array (
-      'replaced' => 
-      array (
-        0 => '*',
-      ),
-    ),
-  ),
 );
