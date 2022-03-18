f05b420a6b8432814273261944e74a50bac3e497
picocodes <picocodes@gmail.com>
2022-03-18 15:21:11 +0300

    ticket compat additions

--- includes/admin/class-getpaid-admin.php
+++ includes/admin/class-getpaid-admin.php
@@ -102,7 +102,7 @@ class GetPaid_Admin {
         }
 
         // General styles.
-        if ( false !== stripos( $page, 'wpi' ) || false !== stripos( $page, 'getpaid' ) || 'gp-setup' == $page || 'geodir-tickets' == $page ) {
+        if ( false !== stripos( $page, 'wpi' ) || false !== stripos( $page, 'getpaid' ) || 'gp-setup' == $page || false !== stripos( $page, 'geodir-tickets' ) ) {
 
             // Styles.
             $version = filemtime( WPINV_PLUGIN_DIR . 'assets/css/admin.css' );
