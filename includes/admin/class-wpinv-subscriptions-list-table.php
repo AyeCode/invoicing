f5fd14c1786a79bc2e7f3a87a7541801b8d2ac14
Kiran Prajapati <officeofkiran@gmail.com>
2024-10-10 20:31:14 +0530

    Fix PHP deprecated errors

--- includes/admin/class-wpinv-subscriptions-list-table.php
+++ includes/admin/class-wpinv-subscriptions-list-table.php
@@ -391,9 +391,8 @@ class WPInv_Subscriptions_List_Table extends WP_List_Table {
 
 		if ( ! empty( $item ) ) {
 			$link = get_edit_post_link( $item );
-			$link = esc_url( $link );
 			$name = esc_html( get_the_title( $item ) );
-			return wpinv_current_user_can_manage_invoicing() ? "<a href='$link'>$name</a>" : $name;
+			return wpinv_current_user_can_manage_invoicing() ? "<a href='" . ( $link ? esc_url( $link ) : '#' ) . "'>$name</a>" : $name;
 		} else {
 			return sprintf( __( 'Item #%s', 'invoicing' ), $item_id );
 		}
