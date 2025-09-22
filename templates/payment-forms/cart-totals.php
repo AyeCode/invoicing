b4c99868e1df79184a1c6247bf7066d02988fd62
Kiran Prajapati <officeofkiran@gmail.com>
2025-09-22 17:11:06 +0530

    Minor BS5 compatibility changes - CHANGED

--- templates/payment-forms/cart-totals.php
+++ templates/payment-forms/cart-totals.php
@@ -35,32 +35,36 @@ if ( ! wpinv_use_taxes() && isset( $cart_totals['tax'] ) ) {
 
 do_action( 'getpaid_before_payment_form_cart_totals', $form, $cart_totals );
 
+$cols_label = 8;
+$cols_value = 4;
+if ( ! empty( $columns ) ) {
+	if ( count( $columns ) == 5 ) {
+		$cols_label = 9;
+		$cols_value = 3;
+	} else if ( count( $columns ) == 3 ) {
+		$cols_label = 6;
+		$cols_value = 6;
+	}
+}
 ?>
 <style>
-	@media screen and (min-width: 576px) {
-		.bsui .border-sm-left {
-			border-left: 1px solid #dee2e6 !important;
-		}
+@media screen and (min-width: 576px) {
+	.bsui .border-sm-left {
+		border-left: 1px solid #dee2e6 !important;
 	}
+}
 </style>
 <div class='getpaid-payment-form-items-cart-totals'>
 	<div class="row">
-		<div class="col-12 offset-sm-6 col-sm-6 border-sm-left pl-sm-0">
-
+		<div class="col-12 offset-sm-6 col-sm-6 pl-sm-0 ps-sm-0">
 			<?php foreach ( $cart_totals as $key => $label ) : ?>
-
-				<div class="getpaid-form-cart-totals-col getpaid-form-cart-totals-<?php echo esc_attr( $key ); ?> font-weight-bold py-2 px-3 <?php echo 'total' === $key ? 'bg-light' : 'border-bottom'; ?> <?php echo 'tax' === $key && wpinv_display_individual_tax_rates() ? 'getpaid-tax-template d-none' : ''; ?>">
-
+				<div class="getpaid-form-cart-totals-col getpaid-form-cart-totals-<?php echo esc_attr( $key ); ?> font-weight-bold border-left border-start py-2 px-3 <?php echo ( 'subtotal' === $key || 'total' === $key ) ? 'bg-light' : 'border-bottom'; ?> <?php echo 'tax' === $key && wpinv_display_individual_tax_rates() ? 'getpaid-tax-template d-none' : ''; ?>">
 					<div class="form-row row">
-
-						<div class="col-8 pl-sm-0 getpaid-payment-form-line-totals-label">
+						<div class="col-<?php echo (int) $cols_label; ?> getpaid-payment-form-line-totals-label text-right text-end">
 							<?php echo esc_html( $label ); ?>
 						</div>
-
-						<div class="col-4 getpaid-payment-form-line-totals-value getpaid-form-cart-totals-total-<?php echo esc_attr( $key ); ?>">
-
+						<div class="col-<?php echo (int) $cols_value; ?> getpaid-payment-form-line-totals-value getpaid-form-cart-totals-total-<?php echo esc_attr( $key ); ?>">
 							<?php
-
 								// Total tax.
 								if ( in_array( $key, array( 'tax', 'discount', 'subtotal', 'total', 'fees' ), true ) ) {
 									wpinv_the_price( 0, $currency );
@@ -68,18 +72,12 @@ do_action( 'getpaid_before_payment_form_cart_totals', $form, $cart_totals );
 
 								do_action( "getpaid_payment_form_cart_totals_$key", $form );
 							?>
-
 						</div>
-
 					</div>
-
 				</div>
-
 			<?php endforeach; ?>
-
 		</div>
 	</div>
 </div>
-
 <?php
 do_action( 'getpaid_payment_form_cart_totals', $form, $cart_totals );
