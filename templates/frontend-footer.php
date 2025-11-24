b703dbcb8b9706717476258943610b93be287dcb
Rahul Gandhi <squarelinksolutions@gmail.com>
2025-11-24 17:23:23 +0530

    minor changes regarding WP standards

--- templates/frontend-footer.php
+++ templates/frontend-footer.php
@@ -17,7 +17,7 @@ global $aui_bs5;
 		<div class="modal-dialog modal-dialog-centered modal-lg" role="checkout" style="max-width: 650px;">
 			<div class="modal-content">
 				<div class="modal-body">
-					<button type="button" class=" btn-close p-2 getpaid-payment-modal-close d-sm-none" data-<?php echo $aui_bs5 ? 'bs-' : ''; ?>dismiss="modal" aria-label="<?php esc_attr__( 'Close', 'invoicing' ); ?>">
+					<button type="button" class=" btn-close p-2 getpaid-payment-modal-close d-sm-none" data-<?php echo $aui_bs5 ? 'bs-' : ''; ?>dismiss="modal" aria-label="<?php esc_attr_e( 'Close', 'invoicing' ); ?>">
 						<?php if ( empty( $aui_bs5 ) ) : ?>
                             <span aria-hidden="true">×</span>
                         <?php endif; ?>
